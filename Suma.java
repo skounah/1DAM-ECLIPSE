8f1a8d3dc94f52f3a95f3a0bca6096ea4f63d434
skounah <el_skoner@hotmail.com>
2014-11-28 08:44:12 +0100

    Subida java suma

---
+++ Suma.java
@@ -0,0 +1,16 @@
+
+import java.util.*; 
+class Suma {  
+    public static void main (String args[]) {  
+      Scanner tcl = new Scanner(System.in);  
+      int a,b,suma;  
+      System.out.println("Ingresa numero A");  
+      a = tcl.nextInt();  
+      System.out.println("Ingresa numero A");  
+      b = tcl.nextInt();  
+      suma = a + b;  
+      System.out.println(suma);  
+    }  
+	}  
+
+
